39f6239c06cde08a8df4967f0184bd5075b6cf79
Afif Alauddin <afif@redfoxlabs.io>
2023-02-14 15:32:40 +0800

    add logger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const ping = require('ping');
 const cron = require('node-cron');
 const fetch = require('node-fetch');
+const winston = require('winston');
+require('winston-daily-rotate-file');
 
 //split from .env file by ,
 const HOSTS = (process.env.HOSTS || '').split(',');
@@ -9,14 +11,47 @@ const HOSTS = (process.env.HOSTS || '').split(',');
 const HTTP_API_TOKEN = process.env.HTTP_API_TOKEN || '';
 const CHAT_ID = process.env.CHAT_ID || '';
 
+const MONITOR_DOWN_ONLY = process.env.MODE === 'DOWN' ?? false;
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.DailyRotateFile({
+      dirname: 'logs',
+      filename: 'application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '2m',
+      maxFiles: '3d',
+    }),
+  ],
+});
+
 const sendMessageToTelegram = (message) => {
+  logger.info('Sending Notification to Telegram');
   const url = `https://api.telegram.org/bot${HTTP_API_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`;
   fetch(url)
     .then((res) => res.json())
     .then((json) => {
       if (json.ok) {
-        console.log('Message sent to telegram');
+        logger.info('Message sent to telegram');
+      } else {
+        logger.error('Error sending message to telegram', {
+          response: json,
+        });
       }
+    })
+    .catch((err) => {
+      logger.error('Error sending message to telegram', { err });
     });
 };
 
@@ -26,8 +61,14 @@ const checkServerStatus = () => {
       var msg = isAlive
         ? 'host: ' + host + ' is alive'
         : 'host: ' + host + ' is dead';
-      console.log(msg);
-      sendMessageToTelegram(msg);
+      logger.info(msg);
+      if (MONITOR_DOWN_ONLY) {
+        if (!isAlive) {
+          sendMessageToTelegram(msg);
+        }
+      } else {
+        sendMessageToTelegram(msg);
+      }
     });
   });
 };
@@ -36,5 +77,11 @@ cron.schedule(process.env.SCHEDULE || '* * * * *', () => {
   checkServerStatus();
 });
 
-console.log('Monitoring Started');
+logger.info('Monitoring Started - [Environment]', {
+  HOSTS,
+  HTTP_API_TOKEN,
+  CHAT_ID,
+  MONITOR_DOWN_ONLY,
+});
+
 checkServerStatus();
